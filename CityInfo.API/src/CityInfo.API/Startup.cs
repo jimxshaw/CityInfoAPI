121bbc641c0d1404ff816f0598c9f453efab65dd
Jim Shaw <jimxshaw@gmail.com>
2017-03-04 13:13:55 -0600

    Tested json serializing options.

--- CityInfo.API/src/CityInfo.API/Startup.cs
+++ CityInfo.API/src/CityInfo.API/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -16,7 +17,20 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                    .AddJsonOptions(o =>
+                                    {
+                                        if (o.SerializerSettings.ContractResolver != null)
+                                        {
+                                            // Json.NET automatically changes model property names to camel case but we don't want that.
+                                            // We want the JSON to match the pascal case of our model properties so we set 
+                                            // the default naming strategy to null.
+                                            var castedResolver =
+                                                o.SerializerSettings.ContractResolver as DefaultContractResolver;
+
+                                            castedResolver.NamingStrategy = null;
+                                        }
+                                    });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
