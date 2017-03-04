73c19d38e96bf198a6d2da677cb7794315c940b5
Jim Shaw <jimxshaw@gmail.com>
2017-03-04 12:39:39 -0600

    Added PointsOfInterest model class.

--- CityInfo.API/src/CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/src/CityInfo.API/CitiesDataStore.cs
@@ -20,19 +20,58 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "Berlin",
-                    Description = "Capital of Germany"
+                    Description = "Capital of Germany",
+                    PointsOfInterest = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 1,
+                            Name = "Reichstag",
+                            Description = "German government building."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 2,
+                            Name = "The Berlin Wall",
+                            Description = "Remains of the wall that divided East and West Berlin."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Austin",
-                    Description = "Capital of Texas"
+                    Description = "Capital of Texas",
+                                        PointsOfInterest = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 3,
+                            Name = "University of Texas",
+                            Description = "The most prestigious public university in Texas."
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Paris",
-                    Description = "Capital of France"
+                    Description = "Capital of France",
+                                        PointsOfInterest = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto
+                        {
+                            Id = 4,
+                            Name = "Champs-Elysees",
+                            Description = "World famous avenue in Paris."
+                        },
+                        new PointOfInterestDto
+                        {
+                            Id = 5,
+                            Name = "The Louvre",
+                            Description = "Museum of art."
+                        }
+                    }
                 },
             };
         }
