6f42a68096bfe7ce3dedfe98ef534210bec3e8a5
Jim Shaw <jimxshaw@gmail.com>
2017-03-05 19:39:41 -0600

    Added HttpPut action.

--- CityInfo.API/src/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/src/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -87,5 +87,44 @@ namespace CityInfo.API.Controllers
                                     },
                                     finalPointOfInterest);
         }
+
+        [HttpPut("{cityId}/pointsofinterest/{id}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int id, [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null)
+            {
+                return BadRequest();
+            }
+
+            if (pointOfInterest.Name == pointOfInterest.Description)
+            {
+                ModelState.AddModelError("Description", "The description should be different from the name");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(p => p.Id == id);
+
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            pointOfInterestFromStore.Name = pointOfInterest.Name;
+            pointOfInterestFromStore.Description = pointOfInterest.Description;
+
+            // This means the request completed successfully but there's nothing to return.
+            return NoContent();
+        }
     }
 }
