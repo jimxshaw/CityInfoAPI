25efa4646c8e34a3ab68a10690c4479ad51f1451
Jim Shaw <jimxshaw@gmail.com>
2017-06-15 11:43:42 -0500

    Added entity classes for City and PointOfInterest.

---
+++ CityInfo/CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(55)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+
+        // The id that serves as the foreign key can be explicitly
+        // stated like this or EF Core will implicitly figure it out.
+        public int CityId { get; set; }
+
+    }
+}
