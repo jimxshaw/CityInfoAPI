403eac3328ba489740b39f182d0519b1364784b8
Jim Shaw <jimxshaw@gmail.com>
2017-06-16 11:53:19 -0500

    Added boilerplate CityInfoRepository.

---
+++ CityInfo/CityInfo.API/Services/CityInfoRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CityInfo.API.Entities;
+
+namespace CityInfo.API.Services
+{
+    public class CityInfoRepository : ICityInfoRepository
+    {
+        private CityInfoContext _context;
+
+        public CityInfoRepository(CityInfoContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<City> GetCities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public City GetCity(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
