df878223c07cf3a98999860d80a25430dc261b57
Jim Shaw <jimxshaw@gmail.com>
2017-06-16 11:26:07 -0500

    Added ICityInfoRepository interface.

---
+++ CityInfo/CityInfo.API/Services/ICityInfoRepository.cs
@@ -0,0 +1,21 @@
+﻿using CityInfo.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Services
+{
+    public interface ICityInfoRepository
+    {
+        IEnumerable<City> GetCities();
+
+        City GetCity(int cityId);
+
+        IEnumerable<PointOfInterest> GetPointsOfInterest(int cityId);
+
+        PointOfInterest GetPointOfInterestForCity(int cityId, int pointOfInterestId);
+
+    }
+}
